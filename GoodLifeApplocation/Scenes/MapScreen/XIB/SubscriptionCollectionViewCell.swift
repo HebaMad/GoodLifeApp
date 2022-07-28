d1ffc520f03b72b5afc4b3691cce38dc53e5430f
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-07-28 03:46:40 +0300

    add subscribe nib

---
+++ GoodLifeApplocation/Scenes/MapScreen/XIB/SubscriptionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionCollectionViewCell.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 27/07/2022.
+//
+
+import UIKit
+
+class SubscriptionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
