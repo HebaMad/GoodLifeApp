2bf2a353da7db90b6e456c27616485bb72fe1802
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-07-28 12:25:04 +0300

    on boarding screens

--- GoodLifeApplocation/Scenes/OnBoardingVC/OnBoardingVC.swift
+++ GoodLifeApplocation/Scenes/OnBoardingVC/OnBoardingVC.swift
@@ -8,22 +8,62 @@
 import UIKit
 
 class OnBoardingVC: UIViewController {
-
+    
+    @IBOutlet weak var stepsIndicator: StepIndicatorView!
+    
+    @IBOutlet weak var continueAction: UIButtonDesignable!
+    @IBOutlet weak var contsinerView: UIView!
+    @IBOutlet weak var skipAction: UIButton!
+    var onBoardingScreen = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        checkStatus()
+
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func continueBtn(_ sender: Any) {
+        checkStatus()
     }
-    */
+    func checkStatus(){
+        switch self.onBoardingScreen{
+
+        case 0:
+            stepsIndicator.currentStep = 0
+            let vc=OnBoardingFrame2()
+            self.addChild(vc)
+            self.contsinerView.addSubview(vc.view)
+            self.onBoardingScreen=1
+        case 1:
+            stepsIndicator.currentStep = 1
+
+            let vc=OnBoardingFrame3()
+            self.addChild(vc)
+            self.contsinerView.addSubview(vc.view)
+            self.onBoardingScreen=2
+        case 2:
+            stepsIndicator.currentStep = 2
+
+            let vc=OnBoardingFrame4()
+            self.addChild(vc)
+            self.contsinerView.addSubview(vc.view)
+            self.onBoardingScreen=3
+        case 3:
+            stepsIndicator.currentStep = 3
+
+            let vc=OnBoardingFrame5()
+            self.addChild(vc)
+//            skipAction.isEnabled = false
+//            skipAction.setTitleColor(.clear, for: .normal)
+            skipAction.setTitleColor(.clear, for: .normal)
+            continueAction.setTitle("Welcome!", for: .normal)
+            self.contsinerView.addSubview(vc.view)
+            self.onBoardingScreen=4
+
+        default:
+  print("test")
+        }
+    }
 
 }
