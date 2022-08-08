a6d2e53ef2219dfeb8e51fd4c2c7e24a759766c3
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-08-08 12:47:10 +0300

    map cell

---
+++ GoodLifeApplocation/Scenes/MapScreen/XIB/FilterCell.swift
@@ -0,0 +1,17 @@
+//
+//  FilterCell.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 08/08/2022.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ GoodLifeApplocation/Scenes/MapScreen/XIB/SpecificFilterCell.swift
@@ -0,0 +1,17 @@
+//
+//  SpecificFilterCell.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 08/08/2022.
+//
+
+import UIKit
+
+class SpecificFilterCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
