f6f49b2b2f9de493d022e7f74b782acfd6785913
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-08-10 18:04:57 +0300

    Add Goal vc

--- GoodLifeApplocation/Scenes/HomeVC/Scene/AddGoalVC.swift
+++ GoodLifeApplocation/Scenes/HomeVC/Scene/AddGoalVC.swift
@@ -1,5 +1,5 @@
 //
-//  AddTask.swift
+//  AddGoalVC.swift
 //  GoodLifeApplocation
 //
 //  Created by heba isaa on 10/08/2022.
@@ -7,26 +7,18 @@
 
 import UIKit
 
-class AddTask: UIViewController {
+class AddGoalVC: UIViewController {
 
+    
     //MARK: - Outlet
     
     @IBOutlet private(set) weak var AddBtn: UIButton!
     @IBOutlet private(set) weak var closeBtn: UIButton!
     @IBOutlet private(set) weak var categorySelectionBtn: UIButton!
-    @IBOutlet private(set) weak var startTimeBtn: UIButton!
-    @IBOutlet private(set) weak var endTimeBtn: UIButton!
-    
+    @IBOutlet private(set) weak var DeadlineBtn: UIButton!
     
     @IBOutlet private(set) weak var titleTxtfield: UITextField!
-    @IBOutlet private(set) weak var alertSwitch: UISwitch!
-    @IBOutlet private(set) weak var timeStartingAlertText: UILabel!
-    @IBOutlet private(set) weak var timeEndingAlertText: UILabel!
-    
-    
-    //MARK: - Properties
-    
-    
+
     
     //MARK: - Life cycle
 
---
+++ GoodLifeApplocation/Scenes/HomeVC/Scene/AddTaskVC.swift
@@ -0,0 +1,41 @@
+//
+//  AddTask.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 10/08/2022.
+//
+
+import UIKit
+
+class AddTaskVC: UIViewController {
+
+    //MARK: - Outlet
+    
+    @IBOutlet private(set) weak var AddBtn: UIButton!
+    @IBOutlet private(set) weak var closeBtn: UIButton!
+    @IBOutlet private(set) weak var categorySelectionBtn: UIButton!
+    @IBOutlet private(set) weak var startTimeBtn: UIButton!
+    @IBOutlet private(set) weak var endTimeBtn: UIButton!
+    
+    
+    @IBOutlet private(set) weak var titleTxtfield: UITextField!
+    @IBOutlet private(set) weak var alertSwitch: UISwitch!
+    @IBOutlet private(set) weak var timeStartingAlertText: UILabel!
+    @IBOutlet private(set) weak var timeEndingAlertText: UILabel!
+    
+    
+    //MARK: - Properties
+    
+    
+    
+    //MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+
+}
