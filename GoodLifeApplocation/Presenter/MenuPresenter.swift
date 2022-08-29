7875fa43d0bef8fec528495bbea582104b2ca5b9
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-08-30 00:52:35 +0300

    add presenter network

--- GoodLifeApplocation/Presenter/MenuPresenter.swift
+++ GoodLifeApplocation/Presenter/MenuPresenter.swift
@@ -6,3 +6,75 @@
 //
 
 import Foundation
+import UIKit
+protocol MenuDelegate{
+    func showAlert(title:String,message:String)
+
+
+}
+typealias menuDelegate = MenuDelegate & UIViewController
+
+class MenuPresenter:NSObject{
+    
+    var delegate:menuDelegate?
+        
+    private func VolunteerOppourtinity(title:String,latitude:String,longitude:String,date:String,time:String,details:String){
+      
+        MenuManager.shared.volunteerOppourtinity(title: title, latitude: latitude, longitude: longitude, date: date, time: time, details: details) { Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    self.delegate?.showAlert(title:"Success", message: response.message)
+
+                }else{
+                    self.delegate?.showAlert(title:"Failure", message: response.message)
+
+                }
+                
+            case let .failure(error):
+                self.delegate?.showAlert(title:"Failure", message: "something wrong try again")
+            }
+        }
+    }
+    
+    
+    private func createIdea(title:String,details:String,time_commitment:String,monthly_revenue:String){
+        MenuManager.shared.createIdea(title: title, details: details, time_commitment: time_commitment, monthly_revenue:monthly_revenue ) { Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    self.delegate?.showAlert(title:"Success", message: response.message)
+
+                }else{
+                    self.delegate?.showAlert(title:"Failure", message: response.message)
+                }
+                
+            case let .failure(error):
+                self.delegate?.showAlert(title:"Failure", message: "something wrong try again")
+            }
+            
+        }
+    }
+    private func createFeedback(title:String,review:String,rate:String,img:Data){
+        MenuManager.shared.createFeedback(title: title, review: review, rate: rate, img: img) { Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    self.delegate?.showAlert(title:"Success", message: response.message)
+
+                }else{
+                    self.delegate?.showAlert(title:"Failure", message: response.message)
+                }
+                
+            case let .failure(error):
+                self.delegate?.showAlert(title:"Failure", message: "something wrong try again")
+            }
+            
+        }
+        
+    }
+    
+}
--- GoodLifeApplocation/Presenter/ProfilePresenter.swift
+++ GoodLifeApplocation/Presenter/ProfilePresenter.swift
@@ -6,3 +6,111 @@
 //
 
 import Foundation
+import UIKit
+
+protocol ProfileDelegate{
+    func showAlert(title:String,message:String)
+
+}
+
+typealias profileDelegate = ProfileDelegate & UIViewController
+
+class ProfilePresenter:NSObject{
+    
+    var delegate:profileDelegate?
+    
+    
+    private func sendContactMsg(msg:String){
+        SettingManager.shared.sendContactMsg(msg: msg) { Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    self.delegate?.showAlert(title:"Success", message: response.message)
+
+                }else{
+                    self.delegate?.showAlert(title:"Failure", message: response.message)
+                }
+                
+            case let .failure(error):
+                self.delegate?.showAlert(title:"Failure", message: "something wrong try again")
+            }
+            
+        }
+    }
+    
+    private func termsAndCondition(callback:@escaping dataCallback ){
+        SettingManager.shared.termsAndCondition { Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    callback(response.data?.url ?? "",true, response.message)
+                
+                }else{
+                    callback(nil,false, response.message)
+                }
+                
+            case let .failure(error):
+                callback(nil,false, error.localizedDescription)
+            }
+        }
+    }
+    
+    private func PrivacyPolicy(callback:@escaping dataCallback ){
+        SettingManager.shared.privacyPolicy { Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    callback(response.data?.url ?? "",true, response.message)
+                
+                }else{
+                    callback(nil,false, response.message)
+                }
+                
+            case let .failure(error):
+                callback(nil,false, error.localizedDescription)
+            }
+        }
+    }
+    
+    private func logout(){
+        SettingManager.shared.logout{ Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    self.delegate?.showAlert(title:"Success", message: response.message)
+                    
+                }else{
+                    self.delegate?.showAlert(title:"Failure", message: response.message)
+                }
+                
+            case let .failure(error):
+                self.delegate?.showAlert(title:"Failure", message: "something wrong try again")
+            }
+        }
+    }
+    
+    private func editProfile(name:String,mobileNumber:String,location:String){
+        SettingManager.shared.editProfile(name: name, mobile: mobileNumber) { Response in
+            switch Response{
+                
+            case let .success(response):
+                if response.status == true{
+                    self.delegate?.showAlert(title:"Success", message: response.message)
+
+                }else{
+                    self.delegate?.showAlert(title:"Failure", message: response.message)
+
+                }
+                
+            case let .failure(error):
+                self.delegate?.showAlert(title:"Failure", message: "something wrong try again")
+
+            }
+        }
+    }
+    
+}
