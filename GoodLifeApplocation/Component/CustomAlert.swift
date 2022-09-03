8746152ed980d755cd897f7356c550ab65bb4d45
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2022-09-03 22:03:33 +0300

    add custom Alert

---
+++ GoodLifeApplocation/Component/CustomAlert.swift
@@ -0,0 +1,20 @@
+//
+//  CustomAlert.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 03/09/2022.
+//
+
+import UIKit
+
+class CustomAlert: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
