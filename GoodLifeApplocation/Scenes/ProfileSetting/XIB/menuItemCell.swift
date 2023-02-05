b8981883b78d7c9e2273bd967dc983e7cb713599
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-02-05 23:32:56 +0200

    new cell design

---
+++ GoodLifeApplocation/Scenes/ProfileSetting/XIB/menuItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  menuItemCell.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 05/02/2023.
+//
+
+import UIKit
+
+class menuItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
