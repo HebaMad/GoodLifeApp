c1201f72195c459e24d100af22c70c80b30e7305
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-03-05 22:30:33 +0200

    add footer cell

---
+++ GoodLifeApplocation/Scenes/ViewOppouritunityVC/XIB/ButtonActionFooterCollectionview.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonActionFooterCollectionview.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 05/03/2023.
+//
+
+import UIKit
+
+class ButtonActionFooterCollectionview: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
