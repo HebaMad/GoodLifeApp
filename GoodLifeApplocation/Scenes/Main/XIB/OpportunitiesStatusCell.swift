6c427fa9b99756631f64afc2020fada88dfb9e05
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-03-14 10:09:33 +0200

    opportunities screen new xib cell

---
+++ GoodLifeApplocation/Scenes/Main/XIB/OpportunitiesStatusCell.swift
@@ -0,0 +1,23 @@
+//
+//  OpportunitiesStatusCell.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 11/03/2023.
+//
+
+import UIKit
+
+class OpportunitiesStatusCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
