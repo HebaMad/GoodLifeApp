7ee6b27033c0073c67e6d6a6b26bbcefc377f4c7
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-03-16 17:20:16 +0200

    add searchviewHeader

---
+++ GoodLifeApplocation/Scenes/Main/XIB/searchviewHeader.swift
@@ -0,0 +1,17 @@
+//
+//  searchviewHeader.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 16/03/2023.
+//
+
+import UIKit
+
+class searchviewHeader: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
