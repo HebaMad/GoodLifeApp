86e2d952f22788a365b1353691a294317c0e66e5
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-03-21 12:29:20 +0200

    add reusableview

---
+++ GoodLifeApplocation/Scenes/Main/XIB/TagSelectionCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  TagSelectionCollectionReusableView.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 20/03/2023.
+//
+
+import UIKit
+
+class TagSelectionCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
