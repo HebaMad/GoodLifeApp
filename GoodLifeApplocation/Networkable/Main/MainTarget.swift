69ef9ea4ede4eb7729af54a0e95693beabb99183
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-03-26 11:45:02 +0300

    add main networkable file

---
+++ GoodLifeApplocation/Networkable/Main/MainTarget.swift
@@ -0,0 +1,8 @@
+//
+//  MainTarget.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 26/03/2023.
+//
+
+import Foundation
---
+++ GoodLifeApplocation/Presenter/MainPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MainPresenter.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 26/03/2023.
+//
+
+import Foundation
