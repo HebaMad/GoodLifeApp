caf54fcba7901cc6dd764a6fb7b6b626f18f0e42
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-03-27 22:54:19 +0300

    add opportunities screens

---
+++ GoodLifeApplocation/Scenes/Main/OpportunitiesCreation/GeneralOpportunitiesCreation.swift
@@ -0,0 +1,29 @@
+//
+//  GeneralOpportunitiesCreation.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 27/03/2023.
+//
+
+import UIKit
+
+class GeneralOpportunitiesCreation: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GoodLifeApplocation/Scenes/Main/OpportunitiesCreation/MarketingOpportunitiesCreation.swift
@@ -0,0 +1,20 @@
+//
+//  MarketingOpportunitiesCreation.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 27/03/2023.
+//
+
+import UIKit
+
+class MarketingOpportunitiesCreation: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+ 
+
+}
