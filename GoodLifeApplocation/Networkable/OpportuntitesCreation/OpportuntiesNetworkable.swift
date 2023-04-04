a34da10ac70aba4bf0a9a6d9b0a004bc94dc92da
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-04-04 12:01:07 +0300

    opportunties networkable+presenter

---
+++ GoodLifeApplocation/Networkable/OpportuntitesCreation/OpportuntiesNetworkable.swift
@@ -0,0 +1,8 @@
+//
+//  OpportuntiesNetworkable.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 03/04/2023.
+//
+
+import Foundation
---
+++ GoodLifeApplocation/Networkable/OpportuntitesCreation/OpportuntiesTarget.swift
@@ -0,0 +1,8 @@
+//
+//  OpportuntiesTarget.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 03/04/2023.
+//
+
+import Foundation
