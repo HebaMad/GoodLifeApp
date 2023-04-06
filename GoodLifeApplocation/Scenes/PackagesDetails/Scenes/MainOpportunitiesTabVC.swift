4769f5b14eed80501954f5ba34345c183a644abd
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-04-06 14:13:15 +0300

    edit in opportunities screen

--- GoodLifeApplocation/Scenes/PackagesDetails/Scenes/MainOpportunitiesTabVC.swift
+++ GoodLifeApplocation/Scenes/PackagesDetails/Scenes/MainOpportunitiesTabVC.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class MainOpportunitiesTabVC: UIViewController {
+class MainOpportunitiesTabVC: ButtonBarPagerTabStripViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupPagerTab()
+        
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        private func setupPagerTab(){
+            settings.style.buttonBarBackgroundColor = .clear
+            settings.style.buttonBarItemBackgroundColor = .clear
+            settings.style.buttonBarItemTitleColor = .white
+            settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+            settings.style.selectedBarHeight = 2.0
+            settings.style.buttonBarMinimumLineSpacing = 30
+            settings.style.buttonBarItemTitleColor = .white
+            settings.style.buttonBarItemsShouldFillAvailableWidth = true
+            settings.style.buttonBarLeftContentInset = 0
+            settings.style.buttonBarRightContentInset = 0
+    
+            changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+                guard changeCurrentIndex == true else { return }
+                oldCell?.label.textColor = UIColor.init(named: "hex")
+                newCell?.label.textColor = UIColor.init(named: "BgColor")
+            }
+        }
+    
+        override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+    
+    
+            let first = GeneralFinicialPagerVC()
+                 first.itemInfo = "Financial Model"
+    
+    
+            let second = GeneralMarketingPageVC.instantiate()
+                second.itemInfo = "Marketing"
+    
+            let third = GeneralLiturgicalPageVC.instantiate()
+            third.itemInfo = "Liturgical"
+    
+    
+            let fourth = DigitalOpportunitiesVC.instantiate()
+                fourth.itemInfo = "Digital"
+    
+            let fifth = SpeificBusinessPlanVC()
+                fifth.itemInfo = "Business Plans"
+    
+            return [first,second,third,fourth,fifth]
+        }
 
 }
--- GoodLifeApplocation/Scenes/PackagesDetails/Scenes/MainOpportuntiesTab.swift
+++ GoodLifeApplocation/Scenes/PackagesDetails/Scenes/MainOpportuntiesTab.swift
@@ -6,58 +6,15 @@
 //
 
 import UIKit
-import XLPagerTabStrip
 class MainOpportuntiesTab:UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        setupPagerTab()
         
     }
 
 
-//    private func setupPagerTab(){
-//        settings.style.buttonBarBackgroundColor = .clear
-//        settings.style.buttonBarItemBackgroundColor = .clear
-//        settings.style.buttonBarItemTitleColor = .white
-//        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
-//        settings.style.selectedBarHeight = 2.0
-//        settings.style.buttonBarMinimumLineSpacing = 30
-//        settings.style.buttonBarItemTitleColor = .white
-//        settings.style.buttonBarItemsShouldFillAvailableWidth = true
-//        settings.style.buttonBarLeftContentInset = 0
-//        settings.style.buttonBarRightContentInset = 0
-//        
-//        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
-//            guard changeCurrentIndex == true else { return }
-//            oldCell?.label.textColor = UIColor.init(named: "hex")
-//            newCell?.label.textColor = UIColor.init(named: "BgColor")
-//        }
-//    }
-//    
-//    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
-//        
-//        
-//        let first = GeneralFinicialPagerVC()
-//             first.itemInfo = "Financial Model"
-// 
-//        
-//        let second = GeneralMarketingPageVC.instantiate()
-//            second.itemInfo = "Marketing"
-//        
-//        let third = GeneralLiturgicalPageVC.instantiate()
-//        third.itemInfo = "Liturgical"
-//        
-//        
-//        let fourth = DigitalOpportunitiesVC.instantiate()
-//            fourth.itemInfo = "Digital"
-//       
-//        let fifth = SpeificBusinessPlanVC()
-//            fifth.itemInfo = "Business Plans"
-//       
-//        return [first,second,third,fourth,fifth]
-//    }
 
 }
 
