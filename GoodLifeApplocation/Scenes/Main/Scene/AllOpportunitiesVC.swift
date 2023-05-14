369929295a1571382fbf21bac09e5ea05876e994
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-05-14 13:59:14 +0300

    view all opportunities

---
+++ GoodLifeApplocation/Scenes/Main/Scene/AllOpportunitiesVC.swift
@@ -0,0 +1,20 @@
+//
+//  AllOpportunities.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 14/05/2023.
+//
+
+import UIKit
+
+class AllOpportunitiesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
