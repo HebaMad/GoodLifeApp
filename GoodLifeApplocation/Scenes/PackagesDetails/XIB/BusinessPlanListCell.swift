eb7068224ab54de5065e980ca9a7287355e9db81
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-07-02 23:01:09 +0300

    edit businessplan

---
+++ GoodLifeApplocation/Scenes/PackagesDetails/XIB/BusinessPlanListCell.swift
@@ -0,0 +1,23 @@
+//
+//  BusinessPlanListCell.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 02/07/2023.
+//
+
+import UIKit
+
+class BusinessPlanListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
