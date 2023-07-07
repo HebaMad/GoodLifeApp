53e95cf0cf74192581183c99dec14ade81f3989c
Heba MAD <71231086+HebaMad@users.noreply.github.com>
2023-07-07 22:58:05 +0300

    add market graph cells

---
+++ GoodLifeApplocation/Scenes/OpportunitiesCreation/XIB/GraphMarketSizeCell.swift
@@ -0,0 +1,23 @@
+//
+//  GraphMarketSizeCell.swift
+//  GoodLifeApplocation
+//
+//  Created by heba isaa on 07/07/2023.
+//
+
+import UIKit
+
+class GraphMarketSizeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
